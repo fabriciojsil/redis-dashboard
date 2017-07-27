3043b09e1c64133ac73c9bee588716e5792b19ca
Fabrício Jean <fabriciojsil@gmail.com>
2017-07-27 20:37:53 -0300

    Preparing test framework and continuous integration

---
+++ README.md
@@ -0,0 +1,3 @@
+## Synopsis
+
+This is a project which will be able to presents important data provides from your Redis node.
---
+++ test/index.test.js
@@ -0,0 +1,10 @@
+const { assert } = require('chai');
+const { expect } = require('chai');
+
+describe('this is an test', ()=>{
+
+	it("my it is running", ()=>{
+		expect(true).to.be.true;
+	});
+	
+});
