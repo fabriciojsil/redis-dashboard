e691b385145cc806ae9e48410291eef4ed8868dd
Fabrício Jean <fabriciojsil@gmail.com>
2017-07-27 21:33:31 -0300

    setting up express

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const PORT = 3000 || process.ENV.PORT;
+let app = express();
+
+app.listen(PORT, (err) => {
+	if(err) console.log(`opss, this need your attention ${err}`);
+	console.log(`app is now running on http://localhost:${PORT}`);
+});
