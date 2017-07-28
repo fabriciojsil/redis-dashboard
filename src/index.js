b9588d420a0951585b8b5b78910a4fb4c815256b
Fabricio Jean <fabricio.silva@resultadosdigitais.com.br>
2017-07-28 12:09:25 -0300

    setting up eslint

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ const PORT = 3000 || process.ENV.PORT;
 let app = express();
 
 app.listen(PORT, (err) => {
-	if(err) console.log(`opss, this need your attention ${err}`);
-	console.log(`app is now running on http://localhost:${PORT}`);
+	if(err){
+		console.log(`opss, this need your attention ${err}`);
+	}else{
+		console.log(`app is now running on http://localhost:${PORT}`);
+	}
 });
